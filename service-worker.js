003d2e56a8ba97b41dab809a30b3b945107c0e39
LilithNML <lilithmorningstar468@gmail.com>
2025-08-05 22:08:43 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,33 @@
-const cacheName = "genshin-pity-cache-v1";
+const cacheName = "genshin-pity-v2";
 const filesToCache = [
   "./",
   "./index.html",
+  "./styles.css",
+  "./script.js",
   "./manifest.webmanifest",
-  "https://fonts.googleapis.com/css2?family=Roboto&display=swap",
-  "https://fonts.googleapis.com/icon?family=Material+Icons"
+  "./icon.png", // Nuevo archivo para el icono
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
+  "https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(cacheName)
-      .then(cache => cache.addAll(filesToCache))
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== cacheName).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(resp => resp || fetch(event.request))
   );
 });
